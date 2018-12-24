7b237d2ac7609b475c930e42dff012d08f1171df
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-12-24 16:30:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,56 +4,40 @@ const client = new Discord.Client({
   fetchAllMembers: true,
 });
 
-var config = require("./config");
-var fs = require('fs');
+const fs = require("fs");
+
+let config = require("./config");
 
 client.website = require("./website/dashboard");
 client.config = config;
-
 client.commands = new Discord.Collection();
 
 fs.readdir("./commandes/", (err, files) => {
-
-  if(err) console.log(err);
-
+  if(err) throw err;
   let commandes = files.filter(f => f.split(".").pop() === "js");
-
-  if(commandes.length <= 0) return console.log("Aucunes commandes trouvées :/");
-
+  if(commandes.length <= 0) return console.log("[ALERTE] Aucune commande de trouvé.");
 
   commandes.forEach((f, i) =>{
-
     let commande = require(`./commandes/${f}`);
-
-    console.log(`${f} commande chargée!`);
-
+    console.log(`[COMMANDE] ${f} chargée!`);
     client.commands.set(commande.help.name, commande);
-
   });
 });
 
-fs.readdir('./events/', (err, files) => {
-
+fs.readdir("./events/", (err, files) => {
   if (err) throw err;
-
   console.log(`Nombre d\'event en chargement ${files.length}`);
 
   files.forEach((f) => {
-
     const events = require(`./events/${f}`);
-    const event = f.split('.')[0];
-
+    const event = f.split(".")[0];
     client.on(event, events.bind(null, client));
-
     setInterval(() => {
-
       delete require.cache[require.resolve(`./events/${f}`)];
-
     }, 2000) 
-
   });
 });
 
 client.login(config.BOT_TOKEN);
-
 module.exports = client;
+

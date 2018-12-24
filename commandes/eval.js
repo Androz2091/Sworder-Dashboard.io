f5aa09190525e00b0b08d92ec3e83d1b068159e7
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-12-24 16:36:44 +0100

    Update eval.js

--- commandes/eval.js
+++ commandes/eval.js
@@ -1,37 +1,28 @@
 const Discord = require("discord.js");
+const util = require("util");
 
+module.exports.run = async(client, msg) => {
 
-module.exports.run = async (client, msg) => {
+    let code = msg.content.split(" ").slice(1);
 
-var util = require("util");
+    if(msg.author.id != client.config.BOT_OWNER_ID) return msg.channel.send("❌ Vous n'avez pas les droits suffisants pour faire cette commande.");
+        try {
+            let ev = eval(code);
+            let str = util.inspect(ev, {
+                depth: 1
+            });
 
-let code = msg.content.split(" ").slice(1);
+            str = `${str.replace(new RegExp(`${client.token}`, "g"), "token")}`;
 
-if(msg.author.id != client.config.BOT_OWNER_ID) return msg.channel.send("❌ Vous n'avez pas les droits nécessaire pour faire cette commande.");
+            if(str.length > 1900) {
+                str = str.substr(0, 1900);
+                str = str + "...";
+            }
 
-  try {
-
-                let ev = eval(code)
-
-                let str = util.inspect(ev, {
-                    depth: 1
-                })
-
-                 str = `${str.replace(new RegExp(`${client.token}`, "g"), "token")}`;
-
-                if(str.length > 1900) {
-                    str = str.substr(0, 1900)
-                    str = str + "..."
-                }
-
-        msg.channel.send('✅ Evaluation réussi:\n'+ '\`\`\`JS\n' + str + '\`\`\`');
-
-    } catch (err) {
-
-        msg.channel.send('❌ Evaluation échoué:\n'+'\`\`\`JS\n' + err + '\`\`\`');
-
-    }
-
+            msg.channel.send(`✅ Evaluation réussi:\n\`\`\`JS\n${str}\`\`\``);
+        } catch (err) {
+            msg.channel.send(`❌ Evaluation échoué:\n\`\`\`JS\n${err}\`\`\``);
+        }
 }
 
 

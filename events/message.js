7c43fdcb71dbf76b10deb03ff915ff45b7153da3
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-12-24 16:35:24 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,14 +1,9 @@
 module.exports = async (client, msg) => {
-
-if(msg.author.bot) return;
-if(msg.channel.type === "dm") return;
-
-if (!msg.content.startsWith(client.config.BOT_PREFIX)) return; 
-
-  let cmd = msg.content.slice(1).split(' ').shift().toLowerCase(); 
-  let commande = client.commands.get(cmd);
-
-
-  if(commande) commande.run(client, msg);
-
+  if(msg.author.bot) return;
+  if(msg.channel.type === "dm") return;
+  if (!msg.content.startsWith(client.config.BOT_PREFIX)) return; 
+    let cmd = msg.content.slice(1).split(' ').shift().toLowerCase(); 
+    let commande = client.commands.get(cmd);
+    if(commande) commande.run(client, msg);
 }
+

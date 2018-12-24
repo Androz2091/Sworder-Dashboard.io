e729bc9b6b1de8870f38d91e51ccf92fb6852b21
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-12-24 16:35:57 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,6 @@
 module.exports = async (client) => {
-
-console.log(`${client.user.username} en ligne !`);
-
- await client.website.load(client);
- await client.user.setPresence({ game: { name: `${client.config.BOT_PREFIX}help`}});
-
+    console.log(`${client.user.username} en ligne!`);
+    await client.website.load(client);
+    await client.user.setPresence({ game: { name: `${client.config.BOT_PREFIX}help`}});
 }
+

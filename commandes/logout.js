5c4cf076a147d518bf1e341cb24bbf212c1b5b4a
Sworder <30504258+Sworder71@users.noreply.github.com>
2018-12-24 16:38:51 +0100

    Update logout.js

--- commandes/logout.js
+++ commandes/logout.js
@@ -1,18 +1,11 @@
 module.exports.run = async (client, msg) => {
-
-if(msg.author.id != client.config.BOT_OWNER_ID) return msg.channel.send("❌ Vous n'avez pas les droits nécessaire pour faire cette commande.");
-
-    msg.channel.send("⚙ Arrêt en cours...").then(() => {
-
-        console.log('Hors-ligne');
-
-        client.destroy();
-
-        process.exit()
-	    
-    })
+    if(msg.author.id != client.config.BOT_OWNER_ID) return msg.channel.send("❌ Vous n'avez pas les droits nécessaire pour faire cette commande.");
+        msg.channel.send("⚙ Arrêt en cours...").then(async() => {
+            console.log('Hors-ligne');
+            await client.destroy();
+            await process.exit()
+        })
 }
-
 
 module.exports.help = {
     name : "logout",

7378bbfda34cbde819e60b8f7097553f618f9c77
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-25 12:16:27 +0100

    Update profile.js

--- website/router/profile.js
+++ website/router/profile.js
@@ -5,7 +5,7 @@ const CheckAuth = require('../auth/CheckAuth');
 router.get('/', CheckAuth, async(req, res) => {
     res.render("profile.ejs", {
         status: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : "Se connecter"),
-        client: req.client.server.client.user,
+        client: req.bot.user,
         user: req.user,
         guilds: req.user.guilds.filter(u => (u.permissions & 2146958591) === 2146958591),
         avatarURL:`https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,

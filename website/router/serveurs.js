d85c5944becb71e4690ac7a23d31491aa0e270da
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-25 12:16:02 +0100

    Update serveurs.js

--- website/router/serveurs.js
+++ website/router/serveurs.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const CheckAuth = require('../auth/CheckAuth');
 
 router.get("/:guildID", CheckAuth, (req, res) => {
-    let serv = req.client.server.client.guilds.get(req.params.guildID);
-    if (!serv) return res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${req.client.server.client.user.id}&scope=bot&permissions=-1&guild_id=${req.params.guildID}`);
-    if(!req.client.server.client.guilds.get(req.params.guildID).members.get(req.user.id).hasPermission("MANAGE_GUILD")) return res.redirect("/dashboard");
+    let serv = req.bot.guilds.get(req.params.guildID);
+    if (!serv) return res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${req.bot.user.id}&scope=bot&permissions=-1&guild_id=${req.params.guildID}`);
+    if(!req.bot.guilds.get(req.params.guildID).members.get(req.user.id).hasPermission("MANAGE_GUILD")) return res.redirect("/dashboard");
       res.render("guild.ejs", {
         status: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : "Se connecter"),
-        client: req.client.server.client.user,
+        client: req.bot.client.user,
         user: req.user,
         avatarURL:`https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
         iconURL:`https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=32`,
@@ -18,7 +18,7 @@ router.get("/:guildID", CheckAuth, (req, res) => {
     .post("/:guildID", CheckAuth, async function(req, res) { 
         if(!req.body.send_CHANNELID || req.body.send_CHANNELID === "NOT_SET") return res.send("Erreur, pas de salon spécifié");
         if(!req.body.send_MESSAGE || req.body.send_MESSAGE.length === 0) return res.send("Erreur, pas de message spécifié");
-        await req.client.server.client.guilds.get(req.params.guildID).channels.get(req.body.send_CHANNELID).send(req.body.send_MESSAGE);
+        await req.bot.guilds.get(req.params.guildID).channels.get(req.body.send_CHANNELID).send(req.body.send_MESSAGE);
         await res.redirect(`/serveurs/${req.params.guildID}`);
     });
 

7fee527fc0b657fdf42b450faf1e9324272cabf0
Sworder <30504258+Sworder71@users.noreply.github.com>
2020-01-26 16:53:17 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const { Router } = require('express');
 const CheckAuth = require('../middlewares/CheckAuth');
 
-module.exports.Router = class Home extends Router {
+module.exports.Router = class Auth extends Router {
     constructor() {
         super();
         this.get('/login', passport.authenticate('discord', { failureRedirect: '/' }), (req, res, next) => res.status(200).redirect('/profile'));

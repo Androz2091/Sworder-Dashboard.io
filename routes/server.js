c15f00ee7fc98f686123bd1919af90d86a87eaf3
Androz2091 <androz2091@gmail.com>
2020-07-13 13:01:10 +0200

    :lock: Check users permissions when sending message

--- routes/server.js
+++ routes/server.js
@@ -21,6 +21,7 @@ module.exports.Router = class Server extends Router {
         this.post("/:guildID", [CheckAuth], async (req, res) => {
             if (!req.body.send_CHANNELID || req.body.send_CHANNELID === "NOT_SET") return res.status(400).send("Erreur, pas de salon spécifié !");
             if (!req.body.send_MESSAGE || req.body.send_MESSAGE.length === 0) return res.status(400).send("Erreur, pas de message spécifié !");
+            if (!req.bot.guilds.get(req.params.guildID).members.get(req.user.id).hasPermission("MANAGE_GUILD")) return res.redirect('/profile');
             await req.bot.guilds.get(req.params.guildID).channels.get(req.body.send_CHANNELID).send(req.body.send_MESSAGE);
             await res.redirect(`/server/${req.params.guildID}`);
         });
